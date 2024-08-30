6c6c5cc1c12bf7effae47bf916cc498d4a55dc01
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2024-08-30 12:47:55 +0700

    add default id

--- src/schema/todo-item.ts
+++ src/schema/todo-item.ts
@@ -1,6 +1,7 @@
 import {z} from "zod";
 
 export const TodoItemSchema = z.object({
+  id: z.string().default(() => crypto.randomUUID()),
   title: z.string().min(1),
   description: z.string().optional(),
   completed: z.boolean().default(false),
