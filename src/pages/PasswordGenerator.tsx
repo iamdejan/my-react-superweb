3b617f38d029a4510219b925d5a8bae8b36d2bdb
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2024-09-03 21:56:35 +0700

    finish logic

--- src/pages/PasswordGenerator.tsx
+++ src/pages/PasswordGenerator.tsx
@@ -1,28 +1,82 @@
-import { Collapse, Container, Paper, Slider, Stack, TextField, Typography } from "@mui/material";
+import { Checkbox, Collapse, Container, FormControlLabel, Paper, Slider, Stack, TextField, Typography } from "@mui/material";
 import { JSX, useEffect, useState } from "react";
 import CopyToClipboardButton from "../components/CopyToClipboardButton";
 
 const maxPasswordLength = 30;
-const lowercaseAlphabet = Array.from({ length: 26 }, (_, i) =>
+const lowerCaseAlphabet = Array.from({ length: 26 }, (_, i) =>
   String.fromCharCode("a".charCodeAt(0) + i)
 );
+const upperCaseAlphabet = Array.from({ length: 26 }, (_, i) =>
+  String.fromCharCode("A".charCodeAt(0) + i)
+);
+const numberCharacters = Array.from({ length: 10 }, (_, i) =>
+  String.fromCharCode("0".charCodeAt(0) + i)
+);
+const symbolCharacters = [
+  "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "[", "]", "{", "}", "|", ";", ":", "'", "\"", "<", ">", ",", ".", "?", "/", "~", "`"
+];
 
 export default function PasswordGenerator(): JSX.Element {
   const [passwordLength, setPasswordLength] = useState<number>(0);
   const [generatedPassword, setGeneratedPassword] = useState<string>("");
 
+  // configurations for generated password's characters
+  const [useLowerCase, setUseLowerCase] = useState<boolean>(true);
+  const [useUpperCase, setUseUpperCase] = useState<boolean>(true);
+  const [useNumbers, setUseNumbers] = useState<boolean>(true);
+  const [useSymbols, setUseSymbols] = useState<boolean>(true);
+  const [availableCharacters, setAvailableCharacters] = useState<string[]>([]);
+
   useEffect(() => {
     let pwd = "";
+    if(passwordLength === 0 || availableCharacters.length === 0) {
+      setGeneratedPassword("");
+      return;
+    }
+
     for(let i = 0; i < passwordLength; i++) {
-      const randomIndex = Math.round(Math.random() * 26);
-      const randomChar = lowercaseAlphabet[randomIndex];
+      const randomIndex = Math.round(Math.random() * availableCharacters.length);
+      const randomChar = availableCharacters[randomIndex];
       pwd += randomChar;
     }
     setGeneratedPassword(pwd);
-  }, [passwordLength]);
+  }, [passwordLength, availableCharacters]);
+
+  useEffect(() => {
+    let characters: string[] = [];
+    if (useLowerCase) {
+      characters = characters.concat(lowerCaseAlphabet);
+    }
+    if (useUpperCase) {
+      characters = characters.concat(upperCaseAlphabet);
+    }
+    if (useNumbers) {
+      characters = characters.concat(numberCharacters);
+    }
+    if (useSymbols) {
+      characters = characters.concat(symbolCharacters);
+    }
+    setAvailableCharacters(characters);
+  }, [useLowerCase, useUpperCase, useNumbers, useSymbols]);
 
   function handleScaleUpdate(_e: Event, value: number | number[]): void {
     setPasswordLength(value as number);
+  }
+
+  function handleLowerCaseUpdate(): void {
+    setUseLowerCase(!useLowerCase);
+  }
+
+  function handleUpperCaseUpdate(): void {
+    setUseUpperCase(!useUpperCase);
+  }
+
+  function handleNumbersUpdate(): void {
+    setUseNumbers(!useNumbers);
+  }
+
+  function handleSymbolsUpdate(): void {
+    setUseSymbols(!useSymbols);
   }
 
   return (
@@ -51,7 +105,52 @@ export default function PasswordGenerator(): JSX.Element {
             value={passwordLength}
           />
         </Stack>
+      </Stack>
 
+      <Stack
+        direction="column"
+        gap={1}
+        sx={{
+          maxWidth: "50vw",
+          marginX: "auto",
+          marginBottom: 3,
+        }}
+        justifyItems="center"
+        alignItems="center"
+      >
+        <FormControlLabel
+          control={<Checkbox />}
+          checked={useLowerCase}
+          onChange={handleLowerCaseUpdate}
+          label="Lower Case"
+        />
+        <FormControlLabel
+          control={<Checkbox />}
+          checked={useUpperCase}
+          onChange={handleUpperCaseUpdate}
+          label="Upper Case"
+        />
+        <FormControlLabel
+          control={<Checkbox />}
+          checked={useNumbers}
+          onChange={handleNumbersUpdate}
+          label="Numbers"
+        />
+        <FormControlLabel 
+          control={<Checkbox />} 
+          checked={useSymbols} 
+          onChange={handleSymbolsUpdate} 
+          label="Symbols"
+        />
+      </Stack>
+
+      <Stack
+        gap={3}
+        sx={{
+          maxWidth: "50vw",
+          marginX: "auto",
+        }}
+      >
         <Collapse
           in={generatedPassword !== ""} 
         >
