edfebb556b845a11ee0d74ffd95d32ad9ce719d5
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2024-09-27 11:53:31 +0700

    implement drawer

---
+++ src/components/RootMenu/index.tsx
@@ -0,0 +1,99 @@
+import { AppBar, Button, Container, Drawer, List, ListItem, Toolbar, Typography } from "@mui/material";
+import { FileRoutesByPath, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { JSX, useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import { grey } from "@mui/material/colors";
+
+type RouteLink = {
+  link: keyof FileRoutesByPath,
+  title: string
+};
+
+const routeList: RouteLink[] = [
+  {
+    link: "/about",
+    title: "About"
+  },
+  {
+    link: "/counter",
+    title: "Counter"
+  },
+  {
+    link: "/to-do-list",
+    title: "To-Do List",
+  },
+  {
+    link: "/distance-calculator",
+    title: "Distance"
+  },
+  {
+    link: "/uuid-generator",
+    title: "UUID"
+  },
+  {
+    link: "/ulid-generator",
+    title: "ULID"
+  },
+  {
+    link: "/bmi-calculator/",
+    title: "Body Mass Index"
+  },
+  {
+    link: "/password-generator",
+    title: "Password Generator"
+  },
+  {
+    link: "/calculator",
+    title: "Calculator"
+  },
+  {
+    link: "/json-sorter",
+    title: "JSON Sorter"
+  }
+];
+
+export default function RootMenu(): JSX.Element {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <AppBar position="static" sx={{margin:"0", padding:"0"}}>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Button color="inherit" onClick={() => setOpen(true)}>
+              <MenuIcon />
+            </Button>
+            <Typography variant="h5" component="div" sx={{ marginLeft: "auto" }}>
+              <a href="/" style={{textDecoration: "none", color: "inherit"}}>My React Superweb</a>
+            </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Outlet />
+      <Drawer open={open} onClose={() => setOpen(false)}>
+        <List>
+          {routeList.map((route) => (
+            <ListItem key={route.link}>
+              <Typography
+                component="a"
+                href={route.link}
+                sx={{
+                  textDecoration: "none",
+                  boxShadow: "none",
+                  color: "inherit",
+                  "&:hover": {
+                    backgroundColor: grey[200],
+                  }
+                }}
+              >
+                {route.title}
+              </Typography>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <TanStackRouterDevtools />
+    </>
+  );
+}
