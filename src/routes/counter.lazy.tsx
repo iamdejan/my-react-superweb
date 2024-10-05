6e9340042de908c78d1b59103ec31602024e6fd7
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2024-10-05 18:08:41 +0700

    remove comma

--- src/routes/counter.lazy.tsx
+++ src/routes/counter.lazy.tsx
@@ -2,5 +2,5 @@ import { createLazyFileRoute } from "@tanstack/react-router";
 import Counter from "../pages/Counter";
 
 export const Route = createLazyFileRoute("/counter")({
-  component: Counter,
+  component: Counter
 });
