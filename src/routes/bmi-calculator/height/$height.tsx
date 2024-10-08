5b7a73085f9c6b71a3397c05fff39d2a9675b8d5
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2024-10-08 15:48:38 +0700

    set dark theme for invalid height error page

--- src/routes/bmi-calculator/height/$height.tsx
+++ src/routes/bmi-calculator/height/$height.tsx
@@ -1,7 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { HeightSchema } from "../../../schema/HeightSchema";
+import { lazyRouteOptions } from "../../../config/routes";
 
 export const Route = createFileRoute("/bmi-calculator/height/$height")({
+  ...lazyRouteOptions,
   loader: ({params: {height}}) => {
     return {
       height: HeightSchema.parse(height)
