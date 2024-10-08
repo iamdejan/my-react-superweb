98cabc30d7d790b6eb786997a5552d23818981f1
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2024-10-08 15:50:35 +0700

    remove lazy route options

--- src/routes/bmi-calculator/height/$height.tsx
+++ src/routes/bmi-calculator/height/$height.tsx
@@ -1,9 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { HeightSchema } from "../../../schema/HeightSchema";
-import { lazyRouteOptions } from "../../../config/routes";
 
 export const Route = createFileRoute("/bmi-calculator/height/$height")({
-  ...lazyRouteOptions,
   loader: ({params: {height}}) => {
     return {
       height: HeightSchema.parse(height)
